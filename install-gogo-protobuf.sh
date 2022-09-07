8e32324e0b9fa4a9e0d9cff2058618c1e3f8b071
leoking <dangliuxin@pointone.tech>
2022-09-07 17:41:25 +0800

    feat: 新增 gogo-proto 安装脚本

---
+++ install-gogo-protobuf.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+read -p 'is your go version > 1.16? (y/n,default n)' flag
+get='go get'
+[[ "$flag" == 'y' ]] && get="$get -d"
+
+echo $flag,$get
+
+$get github.com/gogo/protobuf/protoc-gen-gofast
+
+# 下面这些是protoc-gen-gofast的升级版，支持插件
+# 减少指针加快垃圾回收
+# 生成更多代码
+for name in "" fast faster slick;do
+    $get github.com/gogo/protobuf/protoc-gen-gogo$name
+done
+
+$get github.com/gogo/protobuf/gogoproto
+
+# 速度更快，定制化
+
+for name in proto gogoproto;do
+    # 区别在于下载的proto是放在GOPATH/src下还是GOPATH/pkg下
+    # 官方的文档中按照GO111MODULE=off处理的
+    GO111MODULE=off $get github.com/gogo/protobuf/$name
+    GO111MODULE=on $get github.com/gogo/protobuf/$name
+done
+
+$get github.com/gogo/protobuf/jsonpb
+
+# 使用方式
+# 这里有个需要注意的点：如果开启了GO111MODULE则指定的src/xxx/protobuf找不到，因为我们在go get时放在了pkg下面且有版本管理
+# 目前官方暂未解决，所以这里在安装时设置GO111MODULE来兼容
+#protoc -I=. -I=$GOPATH/src -I=$GOPATH/src/github.com/gogo/protobuf/protobuf --{binary}_out=. myproto.proto
+
+# 支持grpc
+#protoc --gofast_out=plugins=grpc:. my.proto
