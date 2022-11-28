27eb0def2a3da763a9fa3d7bca691ba6ad6a817f
leoking <dangliuxin@pointone.tech>
2022-11-28 13:48:57 +0800

    feat: 支持选择当前镜像或迭代镜像

--- publish/calculate-publish-version.sh
+++ publish/calculate-publish-version.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+read -p 'get current image only? (y/n,default n)' onlyCurrent
+
 #read -p 'namespace=' namespace
 
 #[[ -z $namespace ]] && namespace="bud-base-server"
 
-for namespace in bud-base-server bud-handlemq-server;do
+for namespace in bud-base-server bud-handlemq-server budx-base-server;do
 
-    images=`kubectl get pods -n $namespace -o yaml|grep 'image:'|grep 'buddy-images'|awk '{print $2}'|sed 's/397275977064.dkr.ecr.us-west-1.amazonaws.com\/buddy-images\///g'|sed 's/image\://g'|uniq|sort|grep 'v1.'`
+    images=`kubectl get pods -n $namespace -o yaml|grep 'image:'|grep 'buddy-images'|awk '{print $2}'|sed 's/397275977064.dkr.ecr.us-west-1.amazonaws.com\/buddy-images\///g'|sed 's/image\://g'|uniq|sort|grep ':v'`
     for image in $images;do
         name=`echo $image|awk -F':' '{print $1}'`
         major=`echo $image|awk -F'.' '{print $1}'`
@@ -16,8 +18,13 @@ for namespace in bud-base-server bud-handlemq-server;do
         nextMinor=`expr $minor + 1`
         #nextMinor= ((minor++))
         nextVersion="$major.$nextMinor.${patch}_1"
-        echo $image '->' $nextVersion
-        #echo $image
+        if [[ "${onlyCurrent}" == "y" ]];then
+            echo $image
+        else
+            echo $image '->' $nextVersion
+        fi
     done
 
 done
+
+
